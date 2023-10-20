0ceff5b7e3352549a47547b4b3a3d1207b2f0475
strawhat-dev <kn37.dev@gmail.com>
2023-10-20 16:39:38 -0700

    M  .gitignore M  package.json D  src/index.ts A  src/jsonc-fmt.ts M  src/lib.ts

---
+++ src/jsonc-fmt.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env bun
+import type { BunFile } from 'bun';
+import { isatty } from 'tty';
+import { format, map, popItem, writeln } from './lib';
+
+if (!isatty(0)) Bun.stdin.text().then(format).then(writeln);
+else {
+  const args = process.argv.slice(2);
+  const debug = popItem(args, '-d');
+  const write = popItem(args, '-w') && !debug;
+  let [total, start] = [0, +!debug || Bun.nanoseconds()];
+  const handle = (file: BunFile) => async (out: string) => {
+    if (debug) {
+      const ellapsed = (Bun.nanoseconds() - start) / 1_000_000;
+      out = `formatted ${file.name} in ${ellapsed} ms`;
+      (total += ellapsed), (start = Bun.nanoseconds());
+    }
+
+    if (!write) file = Bun.stdout;
+    else if (!(await file.exists())) {
+      return Promise.reject(
+        new Error('Invalid file(s) provided in write mode.', {
+          cause: { file: file.name, args },
+        })
+      );
+    }
+
+    writeln(out, file);
+  };
+
+  for (const file of map(args, Bun.file)) await file.text().then(format).then(handle(file));
+  total && writeln(`formatted ${args.length} file${args.length > 1 ? 's' : ''} in ${total} ms`);
+}
--- src/lib.ts
+++ src/lib.ts
@@ -6,21 +6,30 @@ const indent_width = 2;
 
 const print_width = 100;
 
-export const writeln = async (str: string, target?: BunFile) => (
-  await Bun.write(target || Bun.stdout, `${str}\n`)
+const indent = (i: number) => ' '.repeat(Math.max(0, i) * indent_width);
+
+export const writeln = async (str: string, target: BunFile = Bun.stdout) => (
+  await Bun.write(target, `${str}\n`) as unknown as void
 );
 
-export const map = (arr: any[], callback: (item: any) => any) => {
-  if (!arr) return [];
+export const popItem = <T>(arr: T[], item: T) => {
+  const found = arr.indexOf(item);
+  if (!(~found)) return;
+  arr.splice(found, 1);
+  return item;
+};
+
+export const map = <T, Init extends any[]>(arr: Init, callback: (item: Init[number]) => T) => {
+  if (!arr?.length) return [];
   const n = arr.length;
   for (let i = 0; i < n; ++i) arr[i] = callback(arr[i]);
-  return arr;
+  return arr as T[];
 };
 
 export const format = (json: string) => {
   if (!(json = json?.trim())) return '';
-  const lines = json.split(/\r?\n/);
   const prepend: string[] = [];
+  const lines = json.split(/\r?\n/);
   const comments = new Map<string, string>();
   if (json.includes('//') || json.includes('/*')) {
     visit(json, {
@@ -29,10 +38,10 @@ export const format = (json: string) => {
         const { path } = getLocation(json, offset);
         if (!path.length) prepend.push(comment);
         else {
-          const key = `"${Bun.hash(comment)}"`;
           const pad = indent(path.length);
+          const hash = `"${Bun.hash(comment)}"`;
+          const entry = `${pad}${hash}: ${hash},`;
           const multiline = map(comment.split(/\r?\n/), (str) => `${pad}${str.trim()}`);
-          const entry = `${pad}${key}: ${key},`;
           comments.set(entry, multiline.join('\n'));
           lines.splice(line, multiline.length, entry);
         }
@@ -41,42 +50,41 @@ export const format = (json: string) => {
   }
 
   const ast = parseTree(lines.join('\n'))!;
-  let result = formatNode(ast, 0, `${prepend.join('\n')}${prepend.length ? '\n' : ''}`);
+  prepend.length && prepend.push('');
+  let result = formatNode(ast, prepend.join('\n'));
   for (const [id, comment] of comments) result = result.replace(id, comment);
   return result;
 };
 
-const indent = (i: number) => ' '.repeat(Math.max(0, i) * indent_width);
-
 const formatNode = (
   ast: Node,
-  level = 0,
   data = '',
+  level = 0,
   type = ast?.value ? 'value' : ast?.type,
-): string => ({
+) => ({
   value() {
     let value = ast.value;
     ast.type === 'string' && (value = `"${value}"`);
-    return data + value;
+    return `${data}${value}`;
   },
   property() {
-    const [{ value = '' } = {}, child] = ast.children!;
-    data += `${indent(level)}"${value}": `;
-    return formatNode(child, level, data);
+    const [{ value: prop }, child] = ast.children!;
+    data += `${indent(level)}"${prop}": `;
+    return formatNode(child, data, level);
   },
   object() {
     data += '{\n';
-    data += map(ast.children!, (child) => formatNode(child, level + 1)).join(',\n');
+    data += map(ast.children!, (child) => formatNode(child, '', level + 1)).join(',\n');
     return `${data}\n${indent(level)}}`;
   },
   array() {
     const [i, div] = ast.parent!.length > print_width ? [level, '\n'] : [-1, ''];
     const [pad, sep] = [indent(i + 1), `,${div || ' '}`];
     data += `[${div}`;
-    data += map(ast.children!, (child) => `${pad}${formatNode(child, level + 1)}`).join(sep);
+    data += map(ast.children!, (child) => formatNode(child, pad, level + 1)).join(sep);
     return `${data}${div}${indent(i)}]`;
   },
 }[type as 'value']?.() ?? (() => {
-  const cause = { ast, level, data };
+  const cause = { ast, data, level };
   throw new Error('Invalid JSON', { cause });
 })());
